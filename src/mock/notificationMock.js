f35a6f395dd21b95975a04230c514deee2fe3b52
bigslime892001 <donkorfrancis1000@icloud.com>
2024-03-09 21:28:18 +0000

    little changes in  the  mock file

--- src/mock/notificationMock.js
+++ src/mock/notificationMock.js
@@ -1,4 +1,3 @@
-
 const notification = [
     {
       id: 1,
@@ -7,43 +6,53 @@ const notification = [
       title: "Kim Hayo Send You a Message",
       message:
         "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
-      time: "1 m ago",
+      time: "1m ago",
     },
     {
       id: 2,
       imageUrl:
-        "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "Kim Hayo Send You a Message",
+        "https://images.unsplash.com/photo-1709038459992-f44881f89157?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Kevin Black",
       message:
-        "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
-      time: "1 m ago",
+        "Hi Kevin, are you looking for hotel in Purwokerto? You can check our hotel ....",
+      time: "3m ago",
     },
     {
       id: 3,
       imageUrl:
-        "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "Kim Hayo Send You a Message",
+        "https://images.unsplash.com/photo-1709884421359-08036a9d1c3a?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Francis",
       message:
-        "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
-      time: "1 m ago",
+        "Francis, are you looking for hotel in Purwokerto? You can check our hotel ....",
+      time: "10m ago",
     },
     {
       id: 4,
       imageUrl:
-        "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "Kim Hayo Send You a Message",
+        "https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "The Rock",
       message:
-        "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
-      time: "1 m ago",
+        "Hi Rock, are you looking for hotel in Purwokerto? You can check our hotel ....",
+      time: " 1hr ago",
     },
     {
       id: 5,
       imageUrl:
-        "https://images.unsplash.com/photo-1708696767929-fc5bd50c164c?q=80&w=3328&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "Kim Hayo Send You a Message",
+        "https://images.unsplash.com/photo-1597694491574-740e696ce5dc?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Slime",
       message:
-        "Hi mariana, are you looking for hotel in Purwokerto? You can check our hotel ....",
-      time: "1 m ago",
+        "Hi Slime, are you looking for hotel in Purwokerto? You can check our hotel ....",
+      time: "2hr ago",
+    },
+    {
+      id: 6,
+      imageUrl:
+        "https://images.unsplash.com/photo-1597694491574-740e696ce5dc?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Slime",
+      message:
+        "Hi Slime, are you looking for hotel in Purwokerto? You can check our hotel ....",
+      time: "2hr ago",
     },
   ];
-  export default notification
+  
+  export default notification;
